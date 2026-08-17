645d8330877556a9b7ab10fbaa8c6ddc622efe37
Artemiy <artem.pospelov2018@yandex.ru>
2026-08-17 11:02:37 +0300

    sider

---
+++ sider/src/main.rs
@@ -0,0 +1,30 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        match stream  {
+            Ok(mut stream) => {
+                handle_connection(&mut stream);
+            }
+            Err(e) => {
+                println!("error: {}", e)
+            }
+        }
+    }
+}
+
+fn handle_connection(stream: &mut TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Received: {:?}", buffer);
+
+    let response = "+PONG\r\n";
+
+    stream.write(response.as_bytes()).unwrap();
+
+    stream.flush().unwrap()
+}
